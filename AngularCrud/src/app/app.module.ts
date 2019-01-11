496e2ed90dbf1172655f1720821bf1a18526d534
Md. Toufiqul Islam <sadydic@gmail.com>
2019-01-11 14:44:17 +0600

    Angular routing and navigation

--- AngularCrud/src/app/app.module.ts
+++ AngularCrud/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import  {RouterModule,Routes} from "@angular/router"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee/create-employee.component';
 
+const appRoutes: Routes =[
+  { path: "list",component: ListEmployeesComponent },
+  { path: "create",component: CreateEmployeeComponent },
+  { path: "",redirectTo: "/list",pathMatch:"full" }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes) // To let the router know about the routes configured above,
+    // pass "appRoutes" constant to forRoot(appRoutes) method
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngularCrud/src/app/models/employee.model.ts
@@ -0,0 +1,13 @@
+export class Employee{
+    id : number;
+    name : String;
+    gender: String;
+    email?:String;
+    phoneNumber?: number;
+    contactPreference: String;
+    dateOfBirth: Date;
+    department: String;
+    isActive: boolean;
+    photoPath?: String;
+
+}
